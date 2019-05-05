44397a220a999e592e52adc5924d4562e792aa44
FrancisYL <yiansu66@gmail.com>
2019-05-04 20:17:21 -0500

    complete slice 5

--- src/App.js
+++ src/App.js
@@ -10,6 +10,23 @@ const terms = {
   S: 'Spring'
 };
 
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
+
+const buttonState = selected => (
+  selected ? `button is-success is-selected` : `button`
+);
+
+const TermSelector = ({ state }) => (
+  <div className="field has-addons">
+    { Object.values(terms).map(value => 
+      <button key={ value }
+              className={ buttonState(value === state.term) }
+              onClick={ () => state.setTerm(value) } >
+        { value }
+      </button> ) }
+  </div>
+);
+
 const getCourseTerm = course => (
   terms[course.id.charAt(0)]
 );
@@ -18,19 +35,62 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 );
 
-const Course = ({ course }) => (
+const hasConflict = (course, selected) => (
+  selected.some(selection => courseConflict(course, selection))
+);
+
+const daysOverlap = (course1, course2) => (
+  days.some(day => course1.days.includes(day) && course2.days.includes(day))
+);
+
+const inMinutes = (time) => (
+  (([hh, mm]) => hh * 60 + mm * 1) (time.split(':'))
+);
+
+const timesOverlap = (course1, course2) => (
+  Math.max(inMinutes(course1.start), inMinutes(course2.start))
+    < Math.min(inMinutes(course1.end), inMinutes(course2.end))
+);
+
+const courseConflict = (course1, course2) => (
+  course1 !== course2
+  && getCourseTerm(course1) === getCourseTerm(course2)
+  && daysOverlap(course1, course2)
+  && timesOverlap(course1, course2)
+);
+
+const Course = ({ course, state }) => (
   <li className="menu-item">
-    <button className="button">
+    <button className={ buttonState(state.selected.includes(course)) }
+            onClick={ () => state.toggle(course) }
+            disabled={ hasConflict(course, state.selected) } >
       { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
     </button>
   </li>
 );
 
-const CourseList = ({ courses }) => (
-  <ul className="menu-list buttons">
-    { courses.map(course => <Course key={ course.id } course={ course } />) }
-  </ul>
-);
+const useSelection = () => {
+  const [selected, setSelected] = useState([]);
+  const toggle = (x) => {
+    setSelected(selected.includes(x) ? selected.filter(y => y !== x) : [x].concat(selected));
+  };
+  return [selected, toggle];
+}
+
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  const [selected, toggle] = useSelection();
+  const termCourses = courses.filter(course => term === getCourseTerm(course));
+
+  return (
+    <React.Fragment>
+      <TermSelector state={ { term, setTerm } } />
+      <ul className="menu-list buttons">
+        { termCourses.map(course => <Course key={ course.id } course={ course } state={ { selected, toggle } } />) }
+      </ul>
+    </React.Fragment>
+  );
+};
 
 const App = () => {
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
@@ -44,7 +104,7 @@ const App = () => {
       setSchedule(json);
     }
     fetchSchedule();
-  }, [])
+  }, []);
 
   return (
     <section>
@@ -53,7 +113,7 @@ const App = () => {
         <CourseList courses = { schedule.courses } />
       </div>
     </section>
-  )
+  );
 }
 
 export default App;
